d2abb44a8505596ce257da949cc95bd2c9418e42
Ekan <ekanessien@gmail.com>
2026-02-02 10:27:13 -0700

    Advances in the JavaScript

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-"use strict"; 
+"use strict";
+
+const gameArea = document.querySelector(".game");
+const button = document.querySelector("button");
+let gamePlay = false;
+
+button.addEventListener("click", () => {
+  if (!gamePlay) {
+    gamePlay = true;
+    maker();
+    button.innerHTML = "Check Combo";
+  } else {
+    console.log("checker");
+  }
+});
+
+function maker() {
+  for (let x = 0; x < 6; x++) {
+    let el = document.createElement("input");
+    el.setAttribute("type", "number");
+    el.max = 9;
+    el.min = 0;
+    el.size = 1;
+    el.style.width = "50px";
+    el.classList.add("numb");
+    el.value = 0;
+    el.order = 0;
+    console.log(el);
+    gameArea.appendChild(el);
+  }
+}
