0ad0b6c23aacbafaa78e7bbc64238592ca4f313c
Ekan <ekanessien@gmail.com>
2026-02-02 13:51:52 -0700

    Program Done

--- index.js
+++ index.js
@@ -2,17 +2,50 @@
 
 const gameArea = document.querySelector(".game");
 const button = document.querySelector("button");
+const message = document.querySelector(".message");
+let score = 0;
 let gamePlay = false;
 
 button.addEventListener("click", () => {
   if (!gamePlay) {
     gamePlay = true;
+    score = 0;
+    gameArea.innerHTML = "";
     maker();
+    message.innerHTML = "Guess the combo";
     button.innerHTML = "Check Combo";
   } else {
     console.log("checker");
+    score++;
+    message.innerHTML = "Guesses " + score;
+    const numbers = document.querySelectorAll(".numb");
+    let winCondition = 0;
+
+    for (let i = 0; i < numbers.length; i++) {
+      if (numbers[i].value == numbers[i].correct) {
+        numbers[i].style.backgroundColor = "green";
+        numbers[i].style.color = "white";
+        console.log("Match");
+        winCondition++;
+      } else {
+        let color = numbers[i].value < numbers[i].correct ? "blue" : "red";
+        numbers[i].style.backgroundColor = color;
+        numbers[i].style.color = "black";
+        console.log("No Match");
+      }
+
+      if (winCondition == numbers.length) {
+        gameEnd();
+      }
+    }
   }
 });
+
+function gameEnd() {
+  message.innerHTML = `You solved the combo in ${score == 1 ? `${score} guess` : `${score} guesses`} `;
+  gamePlay = false;
+  button.innerHTML = "Restart Game";
+}
 
 function maker() {
   for (let x = 0; x < 6; x++) {
@@ -23,8 +56,9 @@ function maker() {
     el.size = 1;
     el.style.width = "50px";
     el.classList.add("numb");
+    el.order = x;
+    el.correct = Math.floor(Math.random() * 10);
     el.value = 0;
-    el.order = 0;
     console.log(el);
     gameArea.appendChild(el);
   }
